d73bbf368b52b6a50edcc5aa44643c4ec77898d3
RaubaAlexey <www.rauba@mail.ru>
2017-10-16 21:30:48 +0300

    commit #1 OOP---psychiatric-hospital

---
+++ Psychiatric-hospital/Classes.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections.Generic;
+
+namespace CrazyClinic
+{
+    enum Diagnos
+    {
+        first,
+        second,
+        third,
+        fourth,
+        five
+    }
+
+    class Clinic
+    {
+        public List<Otdel> Otdels = new List<Otdel>();
+        public Clinic(Otdel[] otdels)
+        {
+            foreach(Otdel otdel in otdels)
+            {
+                Otdels.Add(otdel);
+            }
+        }
+    }
+
+    class Otdel
+    {
+        public List<CrazyPeoples> CrazyPeoples = new List<CrazyPeoples>();
+        public Otdel(CrazyPeoples[] peoples)
+        {
+            foreach(CrazyPeoples people in peoples)
+            {
+                CrazyPeoples.Add(people);
+            }
+        }
+    }
+
+
+    class CrazyPeoples
+    {
+        public Dictionary<int, Diagnos> Zapis = new Dictionary<int, Diagnos>();
+        public void Set(int zap, Diagnos diag)
+        {
+            Zapis.Add(zap, diag);
+        }
+
+    }
+}
---
+++ Psychiatric-hospital/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrazyClinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrazyPeoples peop1 = new CrazyPeoples();
+            CrazyPeoples peop2 = new CrazyPeoples();
+            CrazyPeoples peop3 = new CrazyPeoples();
+            CrazyPeoples peop4 = new CrazyPeoples();
+            CrazyPeoples peop5 = new CrazyPeoples();
+            CrazyPeoples peop6 = new CrazyPeoples();
+
+            List<CrazyPeoples> CrazyPeople = new List<CrazyPeoples>(new CrazyPeoples[] { peop1, peop2, peop3, peop4, peop5, peop6 });
+
+            Otdel FirstOtdel = new Otdel(new CrazyPeoples[] { peop1, peop3 });
+            Otdel SecondOtdel = new Otdel(new CrazyPeoples[] { peop2, peop4 });
+            Otdel ThirdOtdel = new Otdel(new CrazyPeoples[] { peop5, peop6 });
+
+            Clinic CliFirst = new Clinic(new Otdel[] { FirstOtdel, SecondOtdel });
+            Clinic CliSecond = new Clinic(new Otdel[] { ThirdOtdel });
+
+            List<Clinic> Clinic = new List<Clinic>();
+            Clinic.Add(CliFirst);
+            Clinic.Add(CliSecond);
+
+            foreach (CrazyPeoples ppls in CrazyPeople)
+            {
+                ppls.Set(1, Diagnos.first);
+                ppls.Set(2, Diagnos.second);
+                ppls.Set(3, Diagnos.third);
+                ppls.Set(4, Diagnos.fourth);
+                ppls.Set(5, Diagnos.five);
+            }
+
+            Clinic.Select(x => x.Otdels);
+            int count = CliFirst.Otdels.Sum(x => x.CrazyPeoples.Sum(y => y.Zapis.Count(z => z.Value == Diagnos.first)));
+            Console.WriteLine("Count crazy people:" + count.ToString());
+        }
+    }
+}
